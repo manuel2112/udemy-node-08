c5b62b7735683d390fe612a591fff22ccf84049d
manuel2112 <manuel2112@hotmail.com>
2019-07-25 12:12:26 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BBDD
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://manuel2112:13357874@cluster0-hhsis.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
